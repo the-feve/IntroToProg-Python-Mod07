8bce5afdf9a6e77193de5ba91728da5aef152065
the-feve <matthew.fevergeon@gmail.com>
2025-08-24 15:34:16 -0700

    Initial Upload

---
+++ Assignment07.py
@@ -0,0 +1,347 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Assignment07
+# Desc: This assignment demonstrates using data classes
+# with structured error handling
+# Change Log: (Who, When, What)
+#   MFevergeon,8/24/2025,Created Script
+# ------------------------------------------------------------------------------------------ #
+import json
+
+# Define the Data Constants
+MENU: str = '''
+---- Course Registration Program ----
+  Select from the following menu:  
+    1. Register a Student for a Course.
+    2. Show current data.  
+    3. Save data to a file.
+    4. Exit the program.
+----------------------------------------- 
+'''
+FILE_NAME: str = "Enrollments.json"
+
+# Define the Data Variables
+students: list = []  # a table of student data
+menu_choice: str  # Hold the choice made by the user.
+
+
+# DONE Create a Person Class
+# DONE Add first_name and last_name properties to the constructor
+# DONE Create a getter and setter for the first_name property
+# DONE Create a getter and setter for the last_name property
+# DONE Create a Student class the inherits from the Person class
+# DONE call to the Person constructor and pass it the first_name and last_name data
+# DONE Override the __str__() method to return Person data
+# DONE add a assignment to the course_name property using the course_name parameter
+# DONE add the getter for course_name
+# DONE add the setter for course_name
+# DONE Override the __str__() method to return the Student data
+
+
+
+# Processing --------------------------------------- #
+class FileProcessor:
+    """
+    A collection of processing layer functions that work with Json files
+
+    ChangeLog: (Who, When, What)
+    RRoot,1.1.2030,Created Class
+    """
+    @staticmethod
+    def read_data_from_file(file_name: str, student_data: list):
+        """ This function reads data from a json file and loads it into a list of dictionary rows
+        then returns the list filled with student data.
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param file_name: string data with name of file to read from
+        :param student_data: list to be filled with student data (not used in this implementation)
+
+        :return: list
+        """
+        student_objects = []
+        try:
+            # Get a list of dictionary rows from the data file
+            file = open(file_name, "r")
+            json_students = json.load(file)
+            # Convert the list of dictionary rows into a list of Student objects
+            student_objects = []
+            # TODO replace this line of code to convert dictionary data to Student data
+            student_objects = [Student(first_name=student["FirstName"],
+                                       last_name=student["LastName"],
+                                       course_name=student["CourseName"])
+                               for student in json_students]
+
+            file.close()
+
+        except Exception as e:
+            IO.output_error_messages(message="Error: There was a problem with reading the file.", error=e)
+
+        finally:
+            if file.closed == False:
+                file.close()
+
+        return student_objects
+
+    @staticmethod
+    def write_data_to_file(file_name: str, student_data: list):
+        """ This function writes data to a json file with data from a list of dictionary rows
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param file_name: string data with name of file to write to
+        :param student_data: list of dictionary rows to be writen to the file
+
+        :return: None
+        """
+
+        try:
+            file = open(file_name, "w")
+            dict_list = []
+            for student in student_data:
+                if isinstance(student, dict):
+                    dict_list.append(student)
+                else:
+                    dict_list.append({
+                        "FirstName": student.first_name,
+                        "LastName": student.last_name,
+                        "CourseName": student.course_name
+                    })
+            json.dump(dict_list, file, indent=4)
+            file.close()
+            IO.output_student_courses(student_data=student_data)
+        except Exception as e:
+            message = "Error: There was a problem with writing to the file.\n"
+            message += "Please check that the file is not open by another program."
+            IO.output_error_messages(message=message,error=e)
+        finally:
+            if file.closed == False:
+                file.close()
+
+
+# Presentation --------------------------------------- #
+
+class IO:
+    """
+    A collection of presentation layer functions that manage user input and output
+
+    ChangeLog: (Who, When, What)
+    RRoot,1.1.2030,Created Class
+    RRoot,1.2.2030,Added menu output and input functions
+    RRoot,1.3.2030,Added a function to display the data
+    RRoot,1.4.2030,Added a function to display custom error messages
+    """
+
+    @staticmethod
+    def output_error_messages(message: str, error: Exception = None):
+        """ This function displays the a custom error messages to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.3.2030,Created function
+
+        :param message: string with message data to display
+        :param error: Exception object with technical message to display
+
+        :return: None
+        """
+        print(message, end="\n\n")
+        if error is not None:
+            print("-- Technical Error Message -- ")
+            print(error, error.__doc__, type(error), sep='\n')
+
+    @staticmethod
+    def output_menu(menu: str):
+        """ This function displays the menu of choices to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+
+        :return: None
+        """
+        print()  # Adding extra space to make it look nicer.
+        print(menu)
+        print()  # Adding extra space to make it look nicer.
+
+    @staticmethod
+    def input_menu_choice():
+        """ This function gets a menu choice from the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :return: string with the users choice
+        """
+        choice = "0"
+        try:
+            choice = input("Enter your menu choice number: ")
+            if choice not in ("1","2","3","4"):  # Note these are strings
+                raise Exception("Please, choose only 1, 2, 3, or 4")
+        except Exception as e:
+            IO.output_error_messages(e.__str__())  # Not passing e to avoid the technical message
+
+        return choice
+
+    @staticmethod
+    def output_student_courses(student_data: list):
+        """ This function displays the student and course names to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param student_data: list of dictionary rows to be displayed
+
+        :return: None
+        """
+
+        print("-" * 50)
+        for student in student_data:
+
+            # TODO Add code to access Student object data instead of dictionary data
+            print(student)
+
+        print("-" * 50)
+
+    @staticmethod
+    def input_student_data(student_data: list):
+        """ This function gets the student's first name and last name, with a course name from the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.1.2030,Created function
+
+        :param student_data: list of dictionary rows to be filled with input data
+
+        :return: list
+        """
+
+        try:
+            student_first_name = input("Enter the student's first name: ")
+            if not student_first_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+            student_last_name = input("Enter the student's last name: ")
+            if not student_last_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+            course_name = input("Please enter the name of the course: ")
+
+            # TODO Replace this code to use a Student objects instead of a dictionary objects
+            student = Student(first_name=student_first_name,
+                               last_name=student_last_name,
+                               course_name=course_name)
+
+            student_data.append(student)
+            print()
+            print(f"You have registered {student_first_name} {student_last_name} for {course_name}.")
+        except ValueError as e:
+            IO.output_error_messages(message="One of the values was the correct type of data!", error=e)
+        except Exception as e:
+            IO.output_error_messages(message="Error: There was a problem with your entered data.", error=e)
+        return student_data
+
+class Person:
+    """
+    A class representing person data.
+
+    Properties:
+    - first_name (str): The person's first name.
+    - last_name (str): The person's last name.
+
+    ChangeLog:
+    - MFevergeon, 8.24.2023: Created the class.
+    """
+
+    def __init__(self, first_name: str = '', last_name: str = ''):
+        self.__first_name = first_name
+        self.__last_name = last_name
+
+    @property  # (Use this decorator for the getter or accessor)
+    def first_name(self):
+        return self.__first_name.title()  # formatting code
+
+    @first_name.setter
+    def first_name(self, value: str):
+        if value.isalpha() or value == "":  # is character or empty string
+            self.__first_name = value
+        else:
+            raise ValueError("The first name should not contain numbers.")
+
+    @property
+    def last_name(self):
+        return self.__last_name.title()  # formatting code
+
+    @last_name.setter
+    def last_name(self, value: str):
+        if value.isalpha() or value == "":  # is character or empty string
+            self.__last_name = value
+        else:
+            raise ValueError("The last name should not contain numbers.")
+
+    def __str__(self):
+        return f'{self.first_name},{self.last_name}'
+    
+class Student(Person):
+    """
+    A class representing student data, inheriting from Person.
+
+    Properties:
+    - gpa (float): The student's GPA.
+
+    ChangeLog:
+    - MFevergeon, 8.24.2023: Created the class.
+    """
+
+    def __init__(self, first_name: str = '', last_name: str = '', course_name: str = ''):
+        super().__init__(first_name=first_name, last_name=last_name)
+        self.__course_name = course_name
+
+    @property
+    def course_name(self):
+        return self.__course_name
+
+    @course_name.setter
+    def course_name(self, value: str):
+        if isinstance(value, str):
+            self.__course_name = value
+        else:
+            raise ValueError("The course name must be a string.")
+
+    def __str__(self):
+        return f'{self.first_name},{self.last_name},{self.course_name}'
+
+
+# Start of main body
+
+# When the program starts, read the file data into a list of lists (table)
+# Extract the data from the file
+students = FileProcessor.read_data_from_file(file_name=FILE_NAME, student_data=students)
+
+# Present and Process the data
+while (True):
+
+    # Present the menu of choices
+    IO.output_menu(menu=MENU)
+
+    menu_choice = IO.input_menu_choice()
+
+    # Input user data
+    if menu_choice == "1":  # This will not work if it is an integer!
+        students = IO.input_student_data(student_data=students)
+        continue
+
+    # Present the current data
+    elif menu_choice == "2":
+        IO.output_student_courses(students)
+        continue
+
+    # Save the data to a file
+    elif menu_choice == "3":
+        FileProcessor.write_data_to_file(file_name=FILE_NAME, student_data=students)
+        continue
+
+    # Stop the loop
+    elif menu_choice == "4":
+        break  # out of the loop
+    else:
+        print("Please only choose option 1, 2, or 3")
+
+print("Program Ended")
